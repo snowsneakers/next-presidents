4ce29205456eca6a8bbaddc91f4738142171de95
Patrick Snowden <patrickbsnowden@outlook.com>
2022-08-09 16:50:18 -0700

    quotes work maybe

--- components/Welcome.js
+++ components/Welcome.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { Autocomplete, TextField } from "@mui/material";
+import Image from "next/image";
 import Link from "next/link";
 
 function Welcome({ presidents }) {
@@ -11,22 +12,38 @@ function Welcome({ presidents }) {
 
      return (
           <section>
-               <h1 className="text-3xl font-bold mb-2">Welcome!</h1>
-               {
-                    <p className="mb-2">
-                         This is the place to learn all things U.S. Presidents.
-                         Visiting the Presidents page will let you learn about
-                         each President. The quiz page will let you test your
-                         President Knowledge. "The president of the United
-                         States is the head of state and head of government of
-                         the United States,indirectly elected to a four-year
-                         term via the Electoral College. The officeholder leads
-                         the executive branch of the federal government and is
-                         the commander-in-chief of the United States Armed
-                         Forces. Since the office was established in 1789, 45
-                         men have served in 46 presidencies." -wikipedia
-                    </p>
-               }
+               <section className="flex items-center justify-center">
+                    <div className="w-2/3">
+                         <h1 className="text-3xl font-bold mb-2">Welcome!</h1>
+
+                         <p className="mb-2">
+                              This is the place to learn all things U.S.
+                              Presidents. Visiting the Presidents page will let
+                              you learn about each President. The quiz page will
+                              let you test your President Knowledge. "The
+                              president of the United States is the head of
+                              state and head of government of the United
+                              States,indirectly elected to a four-year term via
+                              the Electoral College. The officeholder leads the
+                              executive branch of the federal government and is
+                              the commander-in-chief of the United States Armed
+                              Forces. Since the office was established in 1789,
+                              45 men have served in 46 presidencies.""
+                              -wikipedia
+                         </p>
+                    </div>
+                    <div className="w-1/3 flex items-center justify-center">
+                         <div className="w-[200px] h-[200px] relative">
+                              <Image
+                                   src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/Seal_of_the_President_of_the_United_States.svg/1024px-Seal_of_the_President_of_the_United_States.svg.png"
+                                   layout="fill"
+                                   objectFit="cover"
+                                   blurDataURL="https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/Seal_of_the_President_of_the_United_States.svg/1024px-Seal_of_the_President_of_the_United_States.svg.png"
+                                   placeholder="blur"
+                              />
+                         </div>
+                    </div>
+               </section>
                <div className="flex items-center justify-center gap-2 text-white my-5">
                     <Link href="/quiz">
                          <button className="py-1 px-2 bg-[#0A3161] rounded font-semibold">
